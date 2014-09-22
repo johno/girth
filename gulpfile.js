5d180ae0534c0e65fcd99af82d2e2cc5bed418f4
John Otander <johnotander@gmail.com>
2014-09-22 15:12:07 -0600

    Remove redundant documentation.

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,8 @@ gulp.task('css', function() {
       'node_modules/colors.css/css/colors.css',
       'node_modules/padding.css/padding.css',
       'node_modules/margins.css/margins.css',
-      'node_modules/gray/css/gray.css'])
+      'node_modules/gray/css/gray.css',
+      'css/girth.css'])
     .pipe(concat('c.min.css'))
     .pipe(prefix("last 1 version", "> 1%", "ie 8"))
     .pipe(csslint())
